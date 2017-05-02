b36d9ca4300f2828c590276c4123059363edaccf
ananya94 <ananya.2394@gmail.com>
2017-05-02 16:47:03 -0500

    app.js changes

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var app = angular.module("myapp", ['ngRoute']);
 var addr;
+var api;
 var restaurants;
 app.controller("mycontroller", function ($scope) {
     $scope.enter = function ($event) {
         var keyCode = $event.which || $event.keyCode;
         if (keyCode === 13) {
-            addr = document.getElementById('address').value;
-            console.log("hey");
-            var api = "https://developers.zomato.com/api/v2.1/locations?query="+addr+"&apikey=234ee45c527e2306d02951fd1cf82a89";
-            console.log(api);
+            getlocation();
         }
     }
 });
+var getlocation = function($http,$scope){
+    addr = document.getElementById('address').value;
+            console.log("hey");
+            api = "https://developers.zomato.com/api/v2.1/locations?query="+addr+"&apikey=234ee45c527e2306d02951fd1cf82a89";
+            console.log(api);
+            $http.get(api).then(function(response){
+                $scope.location1= response.data.location_suggestions[0].entity_id;
+                console.log($scope.location1);
+            })
+}
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     //$locationProvider.html5Mode(true);
     $routeProvider.when("/", {
