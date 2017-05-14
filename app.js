297f7394e17e02c166e635a49bc9c365db2d49c1
ananya94 <ananya.2394@gmail.com>
2017-05-14 17:47:34 -0500

    cleaner code

--- app.js
+++ app.js
@@ -1,40 +1,86 @@
 var app = angular.module('myapp', ['ngRoute', 'static']);
 var addr;
-var api;
-var hotel, location2;
-app.controller("mycontroller", ['$routeParams','$http', '$scope', function ($routeParams, $http, $scope) {
-//        $scope.enter = function ($event) {
-//            var keyCode = $event.which || $event.keyCode;
-//            if (keyCode === 13) {
-                var addr = $routeParams.city_id;
-//                $http.get( "https://developers.zomato.com/api/v2.1/locations?query=" + addr + "&apikey=234ee45c527e2306d02951fd1cf82a89").then(function (response) {
-//                    $scope.location1 = response.data.location_suggestions[0].entity_id;
-//                    console.log($scope.location1);
-//                    console.log("hi");
-                    var newapi = "https://developers.zomato.com/api/v2.1/search?entity_id=" + addr + "&entity_type=city&apikey=234ee45c527e2306d02951fd1cf82a89&count=40&sort=rating&order=desc";
-                    console.log(newapi);
-                    $http.get(newapi).then(function (responce) {
-                        $scope.hotel = responce.data.restaurants;
-                        console.log($scope.hotel);
-                    })
-                }]);
+var api, map;
+var markers = [];
+app.controller("mycontroller", ['$routeParams', '$http', '$scope', function ($routeParams, $http, $scope) {
+    var map, center_lat, center_long;
+    var addr = $routeParams.city_id;
+    
+    //api for getting the restaurants from the city with the highest rating
+    var newapi = "https://developers.zomato.com/api/v2.1/search?entity_id=" + addr + "&entity_type=city&apikey=234ee45c527e2306d02951fd1cf82a89&count=40&sort=rating&order=desc";
+    $http.get(newapi).then(function (responce) {
+        $scope.hotel = responce.data.restaurants;
+
+//array for storing the lat, long and name of each fo the markers for the restaurants on the map
+        var r = [];
+        $scope.hotel.forEach(function (item) {
+            r.push({
+                latitude: item.restaurant.location.latitude,
+                longitude: item.restaurant.location.longitude,
+                name: item.restaurant.name
+            })
+        });
+
+
+        var sumlat = 0;
+        var sumlong = 0;
+//clauculating the center of the map depending on the city chosen
+        r.forEach(function (item) {
+            sumlat += parseFloat(item.latitude);
+            sumlong += parseFloat(item.longitude);
+        });
+        center_lat = sumlat / r.length;
+        center_long = sumlong / r.length;
+
+//initalizing the map with calculated center        
+        var element = document.getElementById('map');
+        map = new google.maps.Map(element, {
+            zoom: 12,
+            center: new google.maps.LatLng(center_lat, center_long),
+            mapTypeId: 'terrain',
+            tilt: 0
+        });
+       
+//function call for marker placement
+        eqfeed_callback(r);
+
+    })
+    
+    //function to loop through the results array and place a marker for each
+    // set of coordinates.
+    function eqfeed_callback(markers) {
+        map.center = new google.maps.LatLng(center_lat, center_long);
+        for (var i = 0; i < markers.length; i++) {
+            var latLng = new google.maps.LatLng(markers[i].latitude, markers[i].longitude);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                restaurant: {
+                    name: markers[i].name
+                }
+            });
+        
+//creating an infoWindow specific to the restaurant displaying the name
+            var infoWndw = new google.maps.InfoWindow();
+            marker.addListener('mouseover', function(){
+                infoWndw.setContent('<div class="infoWindow">'+ this.restaurant.name+'</div>');
+                infoWndw.open(map, this);
+                activeInfoWindow = infoWndw;
+            });
+            marker.addListener('mouseout', function(){
+                infoWndw.close();
+            });
+        }
+    }
+}]);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         //$locationProvider.html5Mode(true);
-    $routeProvider.when("/", {
+        $routeProvider.when("/", {
             templateUrl: "/main.html"
-        }).when("/LosAngeles", {
-            templateUrl: "templates/LosAngeles.html",
-            controller: "LosAngeles"
-        }).when("/newyork", {
-            templateUrl: "templates/newyork.html",
-            controller: "newyork"
         }).when("/city/:city_id", {
             templateUrl: "templates/city.html",
             controller: "mycontroller"
-        }).when("/chicago/:city_id/:city_name",{
-        templateUrl:"templates/chicago.html",
-        controller: "chicago"
-    });
+        });
 }
 ]);
