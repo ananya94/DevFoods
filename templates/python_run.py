7957be3ca2a46d72f1034c7f947ddf9aff937fa4
ananya94 <ananya.2394@gmail.com>
2017-05-30 19:30:31 -0500

    changes to hello_world by adding python_run which runs select statements to access the items in cuisine_dish table. Also the progress bar (initial draft) is included

--- templates/python_run.py
+++ templates/python_run.py
@@ -1,8 +1,33 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS, cross_origin
 import sqlite3
-conn = sqlite3.connect('restaurants.db')
-c = conn.cursor()
-s= {}
-for record in c.execute('''SELECT cuisine_name,dish_name, dish_price FROM cuisine_dish'''):
-    print(record)
 
-conn.close()
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/')
+def connection():
+    conn = sqlite3.connect('restaurants.db')
+    c = conn.cursor()
+    s= {}
+    for record in c.execute('''SELECT cuisine_name,dish_id,dish_name, dish_price,quantity FROM cuisine_dish'''):
+        if record[0] in s:
+            s[record[0]].append({
+                'dish_id':record[1],
+                'dish_name': record[2],
+                'dish_price': record[3],
+                'quantity': record[4]
+            })
+        else:
+            s[record[0]] = [{
+                'dish_id': record[1],
+                'dish_name': record[2],
+                'dish_price': record[3],
+                'quantity': record[4]
+            }]
+    print(s)
+    conn.close()
+
+    return jsonify(s)
