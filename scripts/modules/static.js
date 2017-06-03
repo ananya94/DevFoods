47aa85e0247074b22bf10a5ac983fb3da32aacc1
ananya94 <ananya.2394@gmail.com>
2017-06-02 21:20:30 -0500

    contact page improved along with clean up of the restaurants and index page

--- scripts/modules/static.js
+++ scripts/modules/static.js
@@ -1,5 +1,5 @@
 //getting 15 restaurants with the highest rating
-var static1 = angular.module("static",[]);
+var static1 = angular.module("static", []);
 static1.controller("newyork", function ($http, $scope) {
     $http.get("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&sort=rating&order=desc&apikey=234ee45c527e2306d02951fd1cf82a89&count=15").then(function (response) {
         $scope.restaurants = response.data.restaurants;
@@ -25,63 +25,55 @@ static1.directive("modalText", function () {
 });
 
 //controller for triggering the search options
-static1.controller("searchRes", function ($http,$scope) {
+static1.controller("searchRes", function ($http, $scope) {
     getlocation();
-    $scope.enter = function ($event) {
-        var keyCode = $event.which || $event.keyCode;
-        if (keyCode === 13) {
-//            getlocation($http,$scope);
-        }
-    }
-     $("body").addClass("container1")
+    $("body").addClass("container1")
 });
- var getlocation = function() {
-    $( "#tags" ).autocomplete({
-      source: function( request, response ) {
-        $.ajax( {
-          url: "https://developers.zomato.com/api/v2.1/cities",
-          dataType: "jsonp",
-          data: {
-            q: request.term,
-            count: 10,
-            apikey: '234ee45c527e2306d02951fd1cf82a89'
-          },
-          success: function( data ) {
-            data = data.locationSuggestions;
-            return_val = [];
-            $.each(data, function(key, val) {
-                return_val.push({label: val.name, city_id: val.id});
-            })
-            response( return_val );
-          }
-        } );
-      },
-      minLength: 3,
-      select: function( event, ui ) {
-          $('#selected_city_id').val(ui.item.city_id);
-      }
+
+var getlocation = function () {
+    $("#tags").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "https://developers.zomato.com/api/v2.1/cities",
+                dataType: "jsonp",
+                data: {
+                    q: request.term,
+                    count: 10,
+                    apikey: '234ee45c527e2306d02951fd1cf82a89'
+                },
+                success: function (data) {
+                    data = data.locationSuggestions;
+                    return_val = [];
+                    $.each(data, function (key, val) {
+                        return_val.push({
+                            label: val.name,
+                            city_id: val.id
+                        });
+                    })
+                    response(return_val);
+                }
+            });
+        },
+        minLength: 3,
+        select: function (event, ui) {
+            $('#selected_city_id').val(ui.item.city_id);
+        }
     });
-  };
+};
 
 function city_redirect() {
-    if($('#selected_city_id').val() != '') // to check if user selected a city
+    if ($('#selected_city_id').val() != '') // to check if user selected a city
         window.location.hash = '#!/city/' + $('#selected_city_id').val();
 }
-//function called for getting the location api for the location options
-//var getlocation = function($http,$scope){
-//    addr = document.getElementById('address').value;
-//            $http.get("https://developers.zomato.com/api/v2.1/cities?q="+addr+"&count=50&apikey=234ee45c527e2306d02951fd1cf82a89").then(function(response){
-//            $scope.location1= response.data.location_suggestions;
-//            });
-//};
 
 //directive for navigation bar
-static1.directive("navBar", function(){
-    return{
+static1.directive("navBar", function () {
+    return {
         restrict: 'E',
         templateUrl: '/templates/navbar.html'
     }
 });
+
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("myDiv").style.display = "block";
